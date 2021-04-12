09d8ecdf9724f5349b96b1cddeb8e7111fc2081c
Benedikt Ziemons <ben@rs485.network>
2021-04-12 14:13:02 +0200

    Modify repo name, siteConfig for my repo

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 set -e
 
 INPUT_BRANCH=${INPUT_BRANCH:-gh-pages}
-INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build/documentation'}
+INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build/rucio-documentation'}
 OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-'gh-pages'}
 
 [ -z "${GITHUB_TOKEN}" ] && {
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -11,19 +11,19 @@
 // List of projects/orgs using your project for the users page.
 const users = [];
 
-const baseUrl = "/documentation/";
+const baseUrl = "/rucio-documentation/";
 
 const siteConfig = {
   title: "Rucio Documentation", // Title for your website.
-  url: "https://rucio.github.io", // Your website URL
+  url: "https://bziemons.github.io", // Your website URL
   baseUrl, // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "documentation",
-  organizationName: "rucio",
+  projectName: "rucio-documentation",
+  organizationName: "bziemons",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
